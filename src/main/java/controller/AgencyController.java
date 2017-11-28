fc5a1422e5ffde93461dc868dd1265c11cbeda68
Aymeric VIAL-GRELIER <aymvial@gmail.com>
2017-11-28 14:51:20 +0100

    update comment

--- src/main/java/controller/AgencyController.java
+++ src/main/java/controller/AgencyController.java
@@ -17,6 +17,12 @@ public class AgencyController extends Application {
 	@PersistenceContext(unitName="myPU")
     private EntityManager entityManager;
 
+	/**
+	 * Createsthe agency based on modelAgency
+	 *
+	 * @param modelAgency The model of the agency you want to create
+	 * @return agency the concrete agency obtained
+	 */
 	public Agency createAgency (Agency modelAgency) {
 		Agency newAgency = new Agency (modelAgency);
 		entityManager.persist(newAgency);
@@ -25,14 +31,34 @@ public class AgencyController extends Application {
 	}
 
 	/**
-	 * TODO : Vérifier comment faire les updates avec les put
+	 * Update the agency matching the modelAgency
+	 *
+	 * @param modelAgency The model of the agency you wont to update
+	 * @return agency the concrete agency obtained
 	 */
-	public Agency updateAgency (Agency agency) {
+	public Agency updateAgency (Agency modelAgency) {
+		Agency agency = entityManager.find(Agency.class, modelAgency.getId());
+		agency.setAddress(modelAgency.getAddress());
+		agency.setBankLink(modelAgency.getBankLink());
+		agency.setBankName(modelAgency.getBankName());
+		agency.setCity(modelAgency.getCity());
+		agency.setIdMotherAgency(modelAgency.getIdMotherAgency());
+		agency.setPhoneNum(modelAgency.getPhoneNum());
+		agency.setName(modelAgency.getName());
+		agency.setRib(modelAgency.getRib());
+		agency.setType(modelAgency.getType());
 		entityManager.merge(agency);
 		entityManager.flush();
 		return agency;
 	}
 
+	/**
+	 * Disable the agency matching idAgency
+	 * IMPORTANT ! DO NOT DELETE THE ENTITY FOR LEGAL REASONS
+	 *
+	 * @param idAgency the id of the agency to manipulate
+	 * @return message
+	 */
 	public String deleteAgency (Integer idAgency) {
 		Agency agencyRet = entityManager.find(Agency.class, idAgency);
 		agencyRet.setStatus(false);
@@ -41,6 +67,12 @@ public class AgencyController extends Application {
 		return ("Agency successfully deleted");
 	}
 
+	/**
+	 * Enable the agency matching idAgency
+	 *
+	 * @param idAgency the id of the agency to manipulate
+	 * @return message
+	 */
 	public String activateAgency (Integer idAgency) {
 		Agency agencyRet = entityManager.find(Agency.class, idAgency);
 		agencyRet.setStatus(true);
@@ -49,15 +81,33 @@ public class AgencyController extends Application {
 		return ("Agency successfully activated");
 	}
 
+	/**
+	 * Returns the agency matching the id provided
+	 *
+	 * @param idAgency the id of the agency to manipulate
+	 * @return the agency matching
+	 */
 	public Agency getAgency (Integer idAgency) {
 		return (entityManager.find(Agency.class, idAgency));
 	}
 
+	/**
+	 * Returns the list of vehicles of the agency matching idAgency
+	 *
+	 * @param idAgency the id of the agency
+	 * @return the list of agency's vehicles
+	 */
 	public List<Vehicle> getAgencyVehicles (Integer idAgency) {
 		Query q = entityManager.createQuery("SELECT * FROM VEHICLE WHERE idAgency="+idAgency);
 		return ((List<Vehicle>)q.getResultList());
 	}
 
+	/**
+	 * UNKNOWN
+	 *
+	 * @param idAgency the id of the agency
+	 * @return UNKNOWN
+	 */
 	public List<Vehicle> getChildAgencies (Integer idAgency) {
 		// Code douteux de la part de charles, à tester...
 		Query q = entityManager.createQuery("SELECT * FROM AGENCY WHERE id_mother_agency="+idAgency);
--- src/main/java/endpoints/AgencyEndpoint.java
+++ src/main/java/endpoints/AgencyEndpoint.java
@@ -9,9 +9,6 @@ import security.SecuredAgency;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
