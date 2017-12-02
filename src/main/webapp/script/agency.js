5d46290a80bd242cc085d682048183fd402bae45
Heloise-fernandes <heloise.fernandesdealmeida@gmail.com>
2017-12-02 12:04:02 +0100

    Possibilité de modifier les données d'une sous agences

--- src/main/webapp/script/agency.js
+++ src/main/webapp/script/agency.js
@@ -8,7 +8,8 @@ agency.controller("agencyMainCtrl",function($rootScope,$scope,$location,$route,$
 	$rootScope.isMother = false;
 	
 	$rootScope.$route = $route;
-	$rootScope.paramIdAgency = $routeParams.idA;
+	$rootScope.currentAgencyView = $routeParams.idA;
+	$rootScope.currentAgencyUpdate = $routeParams.idupdate;
 	
 	$rootScope.getClass = function (path) {
 		  return ($location.path().substr(0, path.length) === path) ? 'active' : '';
@@ -78,8 +79,15 @@ agency.controller("agencyMainPageCtrl",function($scope,$http,$cookies,$rootScope
 		
 		$http.get('api/agency/view/'+id,config).then(
 		   function(response){
-			   $rootScope.listChildAgencies[response.data.id] = response.data;
-			   reloadSubAgencyMenu()
+			   if(response.data.id==$rootScope.MotherAgency.id)
+			   {
+				   $rootScope.MotherAgency = response.data;
+			   }
+			   else{
+				   $rootScope.listChildAgencies[response.data.id] = response.data;
+				   reloadSubAgencyMenu()
+			   }
+			   
 		    },
 		    function(response)
 		    {
@@ -94,11 +102,10 @@ agency.controller("agencyMainPageCtrl",function($scope,$http,$cookies,$rootScope
 		if($rootScope.MotherAgency.idAgency==undefined)
 		{
 			$scope.isMother = true;
+			console.log($rootScope.listChildAgencies);
+			$rootScope.agencyByCity={};
 			$.each($rootScope.listChildAgencies, function(key, child){
 				var city = child.city.toUpperCase();
-				
-				if(!$rootScope.agencyByCity){$rootScope.agencyByCity={};}
-				
 				if(!$rootScope.agencyByCity[city]){$rootScope.agencyByCity[city]=[];}
 				$rootScope.agencyByCity[city].push({"id" : child.id, "name": child.name!=""?child.name:child.address});
 			});
@@ -148,22 +155,56 @@ agency.controller("graphics",function($scope,$http,$cookies,$rootScope){
 	
 }); 
 
-agency.controller("childRegistration",function($scope,$http,$cookies,$route, $routeParams,$location){
+agency.controller("childRegistration",function($scope,$http,$cookies,$route, $routeParams,$location,$rootScope){
 
+	var token = $cookies.get("token");
+	var user = $cookies.getObject("user");
+	$scope.data = {
+		    availableBanks: [
+				{id:'0',name:'CIC',value:'link',},
+				{id:'1',name:'LCL',value:'link',},
+				{id:'2',name:'La caisse d\'épargne',value:'link',},
+				{id:'3',name:'Crédit mutuel',value:'link',},
+				{id:'4',name:'La banque populaire',value:'link',},
+				{id:'5',name:'Crédit agricole',value:'link',},
+				{id:'6',name:'BNP',value:'link',},
+				{id:'7',name:'Société générale',value:'link',},
+				{id:'8',name:'La banque postale',value:'link',},
+			],
+			
+			availableTypes: [
+				{id:'AgenceN',name:'Agence de Location'},
+			],
+		};
+	
 	$scope.isUpdate = false;
+	
 	if(!$route.routes[$location.path()]){
 		$scope.isUpdate = true;
 		$scope.idUpdatedAgency = $routeParams.idupdate;
 		
-		var token = $cookies.get("token");
+		
 		var config = {headers: {'Authorization': 'Bearer ' + token,}};
 		
 		$http.get('api/agency/view/'+$scope.idUpdatedAgency,config).then(
 		   function(response){
-			   $scope.agency = response.data.type
+			   
+			   $scope.agency = response.data
+			   
+			   //Fill address input
 			   var adressWithoutCity = ($scope.agency.address.slice(0,$scope.agency.address.lastIndexOf($scope.agency.city))).trim();
-			   var postal = adressWithoutCity.slice(-5);
-			   var adresse = adressWithoutCity.slice(0, adressWithoutCity.length-6);
+			   $scope.data.addressp1 = adressWithoutCity.slice(0, adressWithoutCity.length-6)
+			   $scope.data.addressp2 =  adressWithoutCity.slice(-5);
+			   
+			   //Fill bank input
+			   $scope.data.selectedBank = $scope.data.availableBanks.find(function(element) {
+									   return element.name == $scope.agency.bankName;
+									 }); 
+			   $scope.data.selectedType = $scope.data.availableTypes.find(function(element) {
+				   return element.id == $scope.agency.type;
+				 }); 
+			   
+			   console.log($scope.data.selectedType);
 			   
 		    },
 		    function(response)
@@ -174,61 +215,57 @@ agency.controller("childRegistration",function($scope,$http,$cookies,$route, $ro
 		
 	}
 	
-	$scope.data = {
-	    availableBanks: [
-			{id:'0',name:'CIC',value:'link',},
-			{id:'1',name:'LCL',value:'link',},
-			{id:'2',name:'La caisse d\'épargne',value:'link',},
-			{id:'3',name:'Crédit mutuel',value:'link',},
-			{id:'4',name:'La banque populaire',value:'link',},
-			{id:'5',name:'Crédit agricole',value:'link',},
-			{id:'6',name:'BNP',value:'link',},
-			{id:'7',name:'Société générale',value:'link',},
-			{id:'8',name:'La banque postale',value:'link',},
-		],
-	    //selectedBank: {id:'0',name:'CIC',value:'link',}
-	};
-	
 	
     $scope.sendFormAgency = function(){
-    	var user = $cookies.getObject("user");
+    	
     	if($scope.isUpdate)
     	{
-    		$scope.agency.bankLink =$scope.agency.selectedBank.value;
-    		$scope.agency.bankName = $scope.agency.selectedBank.name;
-    		$scope.agency.selectedBank = undefined;
+    		$scope.agency.bankLink =$scope.data.selectedBank.value;
+    		$scope.agency.bankName = $scope.data.selectedBank.name;
     		
+    		$scope.agency.address = $scope.data.addressp1+" "+$scope.data.addressp2+" "+$scope.agency.city.toUpperCase();
+    		$scope.agency.city = $scope.agency.city.toUpperCase()
+    		$scope.agency.type = $scope.data.selectedType.id;
+    		
+    		sendUpdate();
     	}
     	else{
-    		sendNew(user);
+    		sendNew();
     	}
     	
     }
     
-    function sendNew(user)
+    function sendNew()
     {
     	if(user && user.isAgency)
     	{
     		var dataToSend = {
     				"id":null,
-    				"type":$scope.agency.type,
-    				"address":$scope.agency.addressp1+" "+$scope.agency.addressp2+" "+$scope.agency.city.toUpperCase(),
+    				"type":$scope.data.selectedType.id,
+    				"address":$scope.data.addressp1+" "+$scope.data.addressp2+" "+$scope.agency.city.toUpperCase(),
     				"idMotherAgency":user.idAgency,
     				"phoneNum":$scope.agency.phoneNum,
     				"city":$scope.agency.city.toUpperCase(),
     				"name":$scope.agency.name,
-    				"bankLink":$scope.agency.selectedBank.value,
-    				"bankName":$scope.agency.selectedBank.name,
+    				"bankLink":$scope.data.selectedBank.value,
+    				"bankName":$scope.data.selectedBank.name,
     				"rib":$scope.agency.rib,
     				"status":null,
     				"transactionList":null,
     		};
+    		
+    		$scope.data.selectedBank = undefined;
+    		$scope.data.addressp1 = undefined;
+    		$scope.data.addressp2 = undefined;
+    		$scope.data.selectedType = undefined;
+    		
     		var config = {headers: {'Authorization': 'Bearer ' + user.token,}};
     		
     		$http.post('api/agency/create/', dataToSend, config)
     		.then(function successCallback(response) {
     			$scope.agency={};
 	        	$scope.registerForm.$setPristine();
+	        	$rootScope.loadOneChildAgency(response.data.id);
 	        	alert("L'agence est créée");
     		}, function errorCallback(data, status, headers) {
     		
@@ -237,17 +274,47 @@ agency.controller("childRegistration",function($scope,$http,$cookies,$route, $ro
     	}
     }
     
-    function sendUpdate(user){
-    	
+    function sendUpdate(){
+    	if(user && user.isAgency)
+    	{
+    		var config = {headers: {'Authorization': 'Bearer ' + user.token,}};
+    		
+    		$http.put('api/agency/edit/', $scope.agency, config)
+    		.then(function successCallback(response) {
+    			$scope.agency={};
+	        	$scope.registerForm.$setPristine();
+	    		
+	        	alert("L'agence est modifier");
+	        	
+	        	if($scope.idUpdatedAgency==user.idAgency)
+	        	{
+	        		$location.path('/agency');
+	        		$rootScope.loadOneChildAgency($scope.idUpdatedAgency);
+	        	}
+	        	else
+	        	{
+	        		$rootScope.loadOneChildAgency($scope.idUpdatedAgency);
+	        		$location.path('/agency/view/chagency/'+$scope.idUpdatedAgency);
+	        	}
+	        	
+    		}, function errorCallback(data, status, headers) {
+    		
+    		});
+    		
+    	}
     }
 
 });
 
-agency.controller("childAgencyView",function($scope,$http,$cookies,$rootScope,$routeParams){
+agency.controller("childAgencyView",function($scope,$http,$cookies,$rootScope,$routeParams,$location){
 
 	$scope.currentIdAgency = $routeParams.idA
-	console.log($rootScope.listChildAgencies);
 	console.log($rootScope.agencyByCity);
+	
+	$scope.changeLocationToUpdatePage=function(){
+		$location.path('/agency/update/'+$scope.currentIdAgency);
+	};
+	
 	//$scope.idMenu = '#'+$rootScope.listChildAgencies[$scope.currentIdAgency].city+'-'+$rootScope.listChildAgencies[$scope.currentIdAgency].name;
 	
 	
