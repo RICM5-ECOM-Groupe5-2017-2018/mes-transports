d1fb03c9a9fdb5a3b8847661f2e3f3c26683b54f
Gilles B <6406.number@gmail.com>
2017-12-05 09:54:12 +0100

    little fix search manages hours

--- src/main/webapp/script/search.js
+++ src/main/webapp/script/search.js
@@ -9,9 +9,14 @@ search.controller('SearchController', ['$scope', '$http', function SearchControl
 	
 	$('input[name="daterange"]').daterangepicker({
 		"startDate": today,
-	    "endDate": new Date(today.getFullYear(), today.getMonth(), today.getDate()+7)
+	    "endDate": new Date(today.getFullYear(), today.getMonth(), today.getDate()+7),
+	    timePicker: true,
+        timePickerIncrement: 30,
+        locale: {
+            format: 'DD/MM/YYYY h:mm'
+        }
 	}, function(start, end, label) {
-	  console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
+	  console.log("New date range selected: " + start.format('YYYY-MM-DD') + " to " + end.format('YYYY-MM-DD') + " (predefined range: " + label + ")");
 	});
 	
 	$scope.searchRes = {};
