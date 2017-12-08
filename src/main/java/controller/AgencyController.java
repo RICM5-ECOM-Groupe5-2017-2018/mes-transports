88e098ad35418621e321c0be13ddb71b8db9c5a0
Heloise-fernandes <heloise.fernandesdealmeida@gmail.com>
2017-12-08 19:25:20 +0100

    Formatage des données pour graphique das gains en fonctino de l'agence

--- src/main/java/controller/AgencyController.java
+++ src/main/java/controller/AgencyController.java
@@ -4,7 +4,7 @@ import javax.ejb.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.transaction.Transaction;
+//import javax.transaction.Transaction;
 
 import JsonEncoders.JsonMessage;
 import model.Agency;
@@ -79,43 +79,7 @@ public class AgencyController {
      * @param end_date
      * @return
      */
-    public List<Transaction> getTransactions(Integer agencyId,String start_date, String end_date){        
-    	List<Vehicle> lv = entityManager.createQuery("Select v FROM Vehicle v WHERE v.idAgency=:id")
-    			.setParameter("id", agencyId)
-    			.getResultList();
-    			
-		List<Rent> all_rents = new LinkedList<Rent>();		
-		for(int i=0;i<lv.size();i++) {
-			List<Rent> lr=entityManager.createQuery("SELECT r FROM Rent r WHERE r.idVehicle=:id")
-			.setParameter("id", lv.get(i).getId())
-			.getResultList();
-			for(int j=0;j<lr.size();j++) {
-				all_rents.add(lr.get(j));
-			}
-		}
-		
-		List<Transaction> all_transaction = new LinkedList<Transaction>();	
-		for(int i=0;i<all_rents.size();i++) {
-			
-			List<Transaction> lt= entityManager.createQuery("SELECT t FROM Transaction t WHERE t.id=:idT AND t.str_date BETWEEN '"+start_date+ "' AND '"+end_date+"'")
-			.setParameter("idT", all_rents.get(i).getTransaction().getId())
-			.getResultList();
-			
-			for(int j=0;j<lt.size();j++) {
-				all_transaction.add(lt.get(j));
-			}
-			
-			
-		}
-		
-		return all_transaction;
-
-    }
-
-
-
-	
-	public List<Transaction> getTransactions(Integer agencyId, String start_date, String end_date){
+    public List<Transaction> getTransactions(Integer agencyId, String start_date, String end_date){
 		return entityManager.createQuery("Select t FROM Transaction t WHERE t.agency.id=:id AND t.str_date BETWEEN '"+start_date+"' AND '" + end_date + "'")
 		.setParameter("id", agencyId)
 		.getResultList();
--- src/main/webapp/script/Agency/viewAgency.js
+++ src/main/webapp/script/Agency/viewAgency.js
@@ -1,9 +1,9 @@
 agency.controller("agencyMainPageCtrl",function($scope,$http,$cookies,$rootScope,$routeParams,$location){
 
-  $scope.start = new Date();
-	$scope.end = new Date($scope.start.getFullYear(),
-										$scope.start.getMonth(),$scope.start.getDate()+7);
-
+    $scope.start = new Date();
+    $scope.end = new Date($scope.start.getFullYear(),$scope.start.getMonth(),$scope.start.getDate()+7);
+    $scope.BenefitByDate={};
+    $scope.BenefitByAdgency = {};
 
 	$scope.setIDParam=function(id){
 		$routeParams.idA = id;
@@ -13,22 +13,22 @@ agency.controller("agencyMainPageCtrl",function($scope,$http,$cookies,$rootScope
 		$location.path('/agency/update/'+$rootScope.MotherAgency.id);
 	};
 
-  $('input[name="daterange"]').daterangepicker({
-		"startDate": $scope.start,
-	    "endDate": $scope.end,
-	    timePicker: true,
+    $('input[name="daterange"]').daterangepicker({
+        "startDate": $scope.start,
+        "endDate": $scope.end,
+        timePicker: true,
         timePickerIncrement: 30,
         "showWeekNumbers": true,
         locale: {
             format: 'DD/MM/YYYY h:mm'
         }
-  }, function(start, end, label) {
-  	  $scope.start = start;
-  		$scope.end = end;
-  		loadAgenciesProfits();
-  });
+    }, function(start, end, label) {
+        $scope.start = start;
+        $scope.end = end;
+        loadAgenciesProfits();
+    });
 
-  $scope.updateGaphs = function(){
+    function updateGaphBenefitByDate(){
 
       var data = {
           labels: ['1', '2', '3', '4', '5', '6'],
@@ -67,96 +67,140 @@ agency.controller("agencyMainPageCtrl",function($scope,$http,$cookies,$rootScope
       ];
 
       new Chartist.Line('#benefitGlobal', data, options, responsiveOptions);
-  }
+    }
 
-  function loadAgenciesProfits(){
-    console.log("update");
-    var promises = [];
-    var rents = {};
-    var config = {headers: {'Authorization': 'Bearer ' + $rootScope.token,}};
+    function updateGaphBenefitByAdgency(){
 
-    promises.push(new Promise(function(resolve, reject){
-      $http.get('api/agency/rents/'+$rootScope.MotherAgency.id+'/'
-          + moment($scope.start).format('YYYY-MM-DD hh:mm:ss') + '/'
-          + moment($scope.end).format('YYYY-MM-DD hh:mm:ss'),config).then(
+        var data = {
+            labels: ['1', '2', '3', '4', '5', '6'],
+            series: [
+                {
+                    data: [1, 2, 3, 5, 8, 13]
+                }
+            ]
+        };
 
-              function(response)
-              {
-                   rents[$rootScope.MotherAgency.id] = [];
-                   $.each(response.data, function(key, child){
-                     child.endDate = moment(child.endDate).format('YYYY-MM-DD hh:mm:ss');
-                     child.startDate = moment(child.startDate).format('YYYY-MM-DD hh:mm:ss');
-                   });
-                   rents[$rootScope.MotherAgency.id].push(response.data);
+        var options = {
+            axisX: {
+                labelInterpolationFnc: function(value) {
+                    return 'Calendar Week ' + value;
+                }
+            }
+        };
+
+        var responsiveOptions = [
+            ['screen and (min-width: 641px) and (max-width: 1024px)', {
+                showPoint: false,
+                axisX: {
+                    labelInterpolationFnc: function(value) {
+                        return 'Week ' + value;
+                    }
+                }
+            }],
+            ['screen and (max-width: 640px)', {
+                showLine: false,
+                axisX: {
+                    labelInterpolationFnc: function(value) {
+                        return 'W' + value;
+                    }
+                }
+            }]
+        ];
+
+        new Chartist.Line('#benefitGlobal', data, options, responsiveOptions);
+    }
+
+    function loadAgenciesProfits(){
+        console.log("update");
+        var promises = [];
+        var rents = {};
+        var config = {headers: {'Authorization': 'Bearer ' + $rootScope.token,}};
+
+        promises.push(new Promise(function(resolve, reject){
+            $http.get('api/agency/transactions/'+$rootScope.MotherAgency.id+'/'
+                + moment($scope.start).format('YYYY-MM-DD hh:mm:ss') + '/'
+                + moment($scope.end).format('YYYY-MM-DD hh:mm:ss'),config).then(
+
+                function(response)
+                {
+                   rents[$rootScope.MotherAgency.id] = response.data
                    resolve("success");
-              },
-              function(response){console.log("Mother failed");reject("failed");}
-          );
-    }));
-
-    $.each($rootScope.listChildAgencies, function(key, child){
-      promises.push(new Promise(function(resolve, reject){
-        $http.get('api/agency/rents/'+child.id+'/'
-            + moment($scope.start).format('YYYY-MM-DD hh:mm:ss') + '/'
-            + moment($scope.end).format('YYYY-MM-DD hh:mm:ss'),config).then(
-
-    		   function(response){
-                 rents[child.id]=[];
-                 $.each(response.data, function(key, child){
-                   child.endDate = moment(child.endDate).format('YYYY-MM-DD hh:mm:ss');
-                   child.startDate = moment(child.startDate).format('YYYY-MM-DD hh:mm:ss');
-                   rents[child.id].push(child);
-                 });
-                 resolve("success");
-             },
-                function(response){console.log("Child "+child.id+" failed");reject("failed");}
+                },
+                function(response){console.log("Mother failed");reject("failed");}
             );
-      }));
-    });
+        }));
 
-    Promise.all(promises).then(values => {
-      console.log(rents);
-    });
+        $.each($rootScope.listChildAgencies, function(key, child){
+            promises.push(new Promise(function(resolve, reject){
+            $http.get('api/agency/transactions/'+child.id+'/'
+                + moment($scope.start).format('YYYY-MM-DD hh:mm:ss') + '/'
+                + moment($scope.end).format('YYYY-MM-DD hh:mm:ss'),config).then(
 
-  }
+                    function(response){
+                     rents[child.id]=response.data;
 
+                     resolve("success");
+                    },
+                    function(response){console.log("Child "+child.id+" failed");reject("failed");}
+                );
+            }));
+        });
 
-  function formatBenefitByDate(transactions){
-    var BenefitByDate = {};
+        Promise.all(promises).then(values => {
+            console.log(rents);
+            //formatBenefitByDate(rents);
+            formatBenefitByAdgency(rents);
+        });
 
-      $.each(transactions, function(key, agency){
-
-          $.each(transactions, function(key, transac) {
-
-            //moment($scope.str_date).format('YYYY-MM-DD')
-              var date = moment(transac.str_date).format('YYYY-MM-DD')
-              if(!BenefitByDate[date]){BenefitByDate[date]=0}
-              BenefitByDate[date]+=transac.amount;
-
-          });
-      });
-
-    console.log(BenefitByDate);
-  }
-
-  function formatBenefitByAdgency(transaction){
-
-      var BenefitByAdgency = {};
-      $.each(transactions, function(key, agency){
-
-          $.each(transactions, function(key, transac) {
-
-              if(!BenefitByAdgency[agency.id]){BenefitByAdgency[agency.id]=0}
-              BenefitByAdgency[agency.id]+=transac.amount;
-
-          });
-      });
-      console.log(BenefitByAdgency);
-
-  }
-
-    $scope.updateGaphs();
+    }
 
 
+    function formatBenefitByDate(transactions){
+        $scope.BenefitByDate = {};
+
+            $.each(transactions, function(key, agency){
+                if(agency.length()!=0){
+
+                    $.each(transactions, function(key, transac) {
+
+                      //moment($scope.str_date).format('YYYY-MM-DD')
+                      var date = moment(transac.str_date).format('YYYY-MM-DD')
+                      if(!$scope.BenefitByDate[date]){$scope.BenefitByDate[date]=0}
+                        $scope.BenefitByDate[date]+=transac.amount;
+
+                    });
+
+                }
+
+            });
+
+        console.log($scope.BenefitByDate);
+    }
+
+    function formatBenefitByAdgency(transactions){
+        console.log("Array");
+        $scope.BenefitByAdgency = {};
+        $.each(transactions, function(key, agency){
+
+            if(agency.length>0) {
+                console.log(agency);
+                
+
+                $.each(agency, function (idTransac, transac) {
+
+                    if (!$scope.BenefitByAdgency[key]) {
+                        $scope.BenefitByAdgency[key] = 0
+                    }
+                    $scope.BenefitByAdgency[key] += transac.amount;
+
+                });
+            }
+        });
+      console.log($scope.BenefitByAdgency);
+
+    }
+
+    //$scope.updateGaphs();
+    //loadAgenciesProfits();
 
 });
